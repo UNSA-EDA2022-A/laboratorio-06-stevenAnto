5b6c78eb50b304eea028be60af22640278eb977a
stevenAnto <ecalcinap@unsa.edu.pe>
2022-07-10 21:16:21 -0500

    agrego null xd

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -8,9 +8,10 @@ public class Exercise1 {
         BST<Integer> tree1 = new BST<Integer>();
         BST<Integer> tree2 = new BST<Integer>();
 
-        Integer array [] = {1, 2,3, 4, 5};
+        Integer array [] = {};
 
         for(Integer value : array){
+	  System.out.println("ingre");
             tree1.insert(value);
             tree2.insert(value);
         }
@@ -21,6 +22,7 @@ public class Exercise1 {
 
 
     public <T extends Comparable<T>> boolean bstSimilares(BST<T> a1, BST<T> a2){
+      if((a1.root==null&&a2.root==null))return true;
       return isSame(a1.root,a2.root);
     }
     public<E extends Comparable<E>> boolean isSame(Node<E> n1,Node<E> n2){
