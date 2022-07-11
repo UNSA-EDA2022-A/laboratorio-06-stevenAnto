71f8eea75eb8ca9fd62306b5725a5b799fca8256
stevenAnto <ecalcinap@unsa.edu.pe>
2022-07-10 22:14:20 -0500

    cuando envio un nodo y nullo no entraba a la la recurrenia

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -28,10 +28,10 @@ public class Exercise1 {
     public<E extends Comparable<E>> boolean isSame(Node<E> n1,Node<E> n2){
       boolean verificador=true;
       if((n1==null&&n2==null)||(n1!=null&&n2!=null)){
-	if(n1.left!=null&&n2.left!=null){
+	if(n1.left!=null||n2.left!=null){
 	  verificador = isSame(n1.left,n2.left);
 	}
-	else if(n1.right!=null&&n2.right!=null){
+	else if(n1.right!=null||n2.right!=null){
 	  verificador = isSame(n1.right,n2.right);
 	}
 	else if(n1.right==null&&n2.right==null){
