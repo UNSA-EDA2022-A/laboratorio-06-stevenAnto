b492dc026f4cbfee763d8e050fbbe2e47c72622e
stevenAnto <ecalcinap@unsa.edu.pe>
2022-07-13 19:25:38 -0500

    se implemtno correcto preorden, se puso dos condiciones para salir de la recursividad

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -8,7 +8,7 @@ public class Exercise2 {
     BST<Integer> tree1 = new BST<Integer>();
     BST<Integer> tree2 = new BST<Integer>();
 
-    Integer array [] = {1, 2,3, 4, 5};
+    Integer array [] = {1, 2,3 };
 
     for(Integer value : array){
       tree1.insert(value);
@@ -23,23 +23,46 @@ public class Exercise2 {
 
   public <T extends Comparable<T>> boolean bstIguales(BST<T> a1, BST<T> a2){
     if(a1.root == null && a2.root ==null)return true;
-    
     return isIgual(a1.root,a2.root);
   }
   public <E extends Comparable<E>> boolean isIgual(Node<E> n1,Node<E> n2){
-    boolean verificador = true;
-    if(n1.left!=null&&n2.left!=null){
-      if(n1.data.compareTo(n2.data)!=0)return false;
-      verificador =isIgual(n1.left,n2.left);
-
-    }else if(n1.left==null&&n2.left==null){
-      if(n1.data.compareTo(n2.data)!=0)return false;
-      if(n1.right!=null&&n2.right!=null){
-      verificador = isIgual(n1.right,n2.right);
+    System.out.println("entro");
+    //if(n1==null&&n2==null);
+    //if((n1==null&&n2!=null)||(n1!=null&&n2==null))return false;
+    if(n1.data.compareTo(n2.data)==0){
+      System.out.println("entro1.2");
+      if(n1.left!=null&&n2.left!=null){
+	System.out.println("entro 1.2.1");
+	return isIgual(n1.left,n2.left);
       }
-    }else{
-      return false;
+      if((n1.left==null&&n2.left!=null)||(n1.left!=null&&n2.left==null)){
+	return false;
+      }
+      if(n1.right!=null&&n2.right!=null){
+	System.out.println("entro 1.2.3");
+	return isIgual(n1.right,n2.right);
+      }
+      if((n1.right==null&&n2.right!=null)||(n1.right!=null&&n2.right==null)){
+	return false;
+      }
     }
-    return verificador;
+    else{
+      return false;
+    } 
+    return true;
   }
 }
+/*boolean verificador = true;
+  if(n1.left!=null&&n2.left!=null){
+  if(n1.data.compareTo(n2.data)!=0)return false;
+  verificador =isIgual(n1.left,n2.left);
+
+  }else if(n1.left==null&&n2.left==null){
+  if(n1.data.compareTo(n2.data)!=0)return false;
+  if(n1.right!=null&&n2.right!=null){
+  verificador = isIgual(n1.right,n2.right);
+  }
+  }else{
+  verificador = false;
+  }
+  return verificador;*/
